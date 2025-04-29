5ceee408625d80d23c8ddd616536c6db8e6480b9
javier c. zuñiga <javier.kamilo007@gmail.com>
2025-04-29 14:59:49 +0000

    se realiza ajuste en el controller de marca para listar marcas registradas

--- src/main/java/com/ganepalmira/pruebas/controller/MarcaController.java
+++ src/main/java/com/ganepalmira/pruebas/controller/MarcaController.java
@@ -1,22 +1,40 @@
 package com.ganepalmira.pruebas.controller;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ganepalmira.pruebas.domain.Marca;
 import com.ganepalmira.pruebas.repository.MarcaRepository;
+import com.ganepalmira.pruebas.service.MarcaService;
 import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/marcas")
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1/marcas")
+@Slf4j
 public class MarcaController {
 
-    private final MarcaRepository marcaRepository;
+    @Autowired
+    private MarcaRepository marcaRepository;
 
-    public MarcaController(MarcaRepository marcaRepository) {
-        this.marcaRepository = marcaRepository;
-    }
+    @Autowired
+	private MarcaService marcaService;
 
-    @GetMapping
+    @GetMapping("/findAll")
     public List<Marca> findAll() {
         try {
             return marcaRepository.findAll();
